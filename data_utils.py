b7f8a3383c2b0e8009243a8232685b38bec7c81e
tian2156 <Y12242156@mail.ecust.edu.cn>
2026-01-05 23:29:10 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -10,7 +10,7 @@ def checkGraphConnectivity(adjMatrix):
 
     while queue:
         currentNode = queue.popleft()
-        neighbors = np.where(adjMatrix[currentNode, :] == 1)[0]
+        neighbors = np.where(adjMatrix[currentNode, :] > 0)[0]
         for neighbor in neighbors:
             if not visited[neighbor]:
                 visited[neighbor] = True
@@ -20,55 +20,47 @@ def checkGraphConnectivity(adjMatrix):
     
     return isConnected, visited
 
-def groupmake(A):
-
+def groupmake_strict(A, groupsize=100):
     group_num = A.shape[0]
-    allgroups = []
-    selected_v = []
-    numbershare = np.zeros((group_num, group_num))
-    for i in range(group_num):
-        startNum = 100 * i
-        endNum = 100 * (i + 1) - 1
-        allgroups.append(list(range(startNum, endNum + 1)))
+    group_vars = [set() for _ in range(group_num)]
+    next_var_id = 0
 
+    # 1. 强制共享变量
     for i in range(group_num):
         for j in range(i + 1, group_num):
-            if A[i, j] != 0:
-                randomNumber = A[i, j]
-                numbershare[i, j] = randomNumber
-                filteredA = [x for x in allgroups[i] if x not in selected_v]
-                indices = np.random.choice(filteredA, randomNumber, replace=False)
-                selected_v.extend(indices)
-                allgroups[j].extend(indices)
-    selected_v = list(set(selected_v))
+            k = int(A[i, j])
+            if k > 0:
+                shared_vars = set(range(next_var_id, next_var_id + k))
+                next_var_id += k
+                group_vars[i].update(shared_vars)
+                group_vars[j].update(shared_vars)
 
-    for i in range(len(allgroups)):
-        currentArray = allgroups[i]
-        filteredArray = [x for x in currentArray if x in selected_v]
-        remainingElements = [x for x in currentArray if x not in selected_v]
-        additionalCount = 100 - len(filteredArray)
-        indices = np.random.choice(remainingElements, additionalCount, replace=False)
-        filteredArray.extend(indices)
-        allgroups[i] = filteredArray
+    # 2. 补齐私有变量
+    for i in range(group_num):
+        needed = groupsize - len(group_vars[i])
+        if needed < 0:
+            raise ValueError(
+                f"group {i} has more than {groupsize} shared variables"
+            )
+        private_vars = set(range(next_var_id, next_var_id + needed))
+        next_var_id += needed
+        group_vars[i].update(private_vars)
 
-    allElements = [item for sublist in allgroups for item in sublist]
-    allElements = list(set(allElements))
-    lengh_allElements = len(allElements)
-    _, newIndex = np.unique(allElements, return_inverse=True)
-    valueMap = dict(zip(allElements, newIndex))
+    # 3. 重新编号到 [0, D)
+    all_vars = sorted(set().union(*group_vars))
+    var_map = {v: idx for idx, v in enumerate(all_vars)}
 
-    for i in range(len(allgroups)):
-        currentArray = [valueMap[val] for val in allgroups[i]]
-        allgroups[i] = currentArray
+    allgroups = []
+    for i in range(group_num):
+        allgroups.append([var_map[v] for v in group_vars[i]])
 
     return allgroups
-
 def computeRotation(D):
     A = np.random.randn(D, D)
     Q, R = np.linalg.qr(A)
     return Q, R
 
-def data_generator(problem_batch_size):
+def data_generator(problem_batch_size,G_num,G_prob):
 
     topo_list = []
     w_list = []
@@ -77,7 +69,8 @@ def data_generator(problem_batch_size):
     allgroups_list = []
     D_list = []
     R100_list = []
-    group_num = 10
+    group_num = G_num  # 10
+    G_prob = G_prob    # 0.1
     
     for func in range(1, problem_batch_size + 1):
 
@@ -88,7 +81,7 @@ def data_generator(problem_batch_size):
         for i in range(group_num):
             for j in range(group_num):
                 if i != j:
-                    A[i, j] = np.random.rand() < 0.1  #链接概率
+                    A[i, j] = np.random.rand() < G_prob  #链接概率
 
         for i in range(group_num):
             if np.all(A[i, :] == 0) and np.all(A[:, i] == 0):
@@ -133,7 +126,7 @@ def data_generator(problem_batch_size):
             w[i] = 10 ** (3 * np.random.normal(0, 1))  #CEC 2010
         w_list.append(w)
 
-        allgroups = groupmake(A)
+        allgroups = groupmake_strict(A, groupsize=100)
         allgroups_list.append(allgroups)
         allElements = list(set([item for sublist in allgroups for item in sublist]))
         D_list.append(len(allElements))
@@ -147,13 +140,19 @@ def data_generator(problem_batch_size):
     
         R100, _ = computeRotation(100)
         R100_list.append(R100)
-
     detail = {
         "allgroups_list": allgroups_list,
         "xopt_list": xopt_list,
-        "xopt_1_list":xopt_1_list,
+        "xopt_1_list": xopt_1_list,
         "D_list": D_list,
         "R100_list": R100_list,
     }
 
-    return np.array(topo_list), np.array(w_list), detail, topo_list, w_list
+    # 使用结构化的字典格式返回，便于后续使用
+    output = {
+        'topology': np.array(topo_list),   # 拓扑矩阵
+        'weights': np.array(w_list),       # 权重
+        'details': detail                  # 详细信息
+    }
+
+    return output
